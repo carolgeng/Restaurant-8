2452e69df562f63cfdb4be958710459e029b88ab
carolgeng <carolgeng27@gmail.com>
2023-05-05 17:41:22 -0500

    change button styling + added comments
    Signed-off-by: carolgeng <carolgeng27@gmail.com>

--- edititem.php
+++ edititem.php
@@ -1,17 +1,16 @@
 <!-- Carol Geng, entire file -->
-<!-- This is the delete function for items to be deleted from the menu which is only allowed for the admin-->
+<!-- This displays the information for the items if you're the admin. The items can be edited or an item can be added here-->
 
 <?php
 session_start();
     include("db.php");
     include("header.php");
 
+    // check if you're admin
     $user_data = check_login($conn);
-    // if (!empty($_GET['type']) && !empty($_GET['id'])) {
-    //     change_type($conn, $_GET['id'], $_GET['type']);
-    // }
+    
 ?>
-
+<!-- css styling -->
 <!DOCTYPE html>
 <html>
     <head>
@@ -19,45 +18,49 @@ session_start();
         
     </head>
     <body>
-        <div class="container">
-            <form action="additem.php">
-                <button class="btn btn-primary" >Add Item</button>
-            </form>
-            
-        </div>
-        <br>
-        <div class="container">
-            <table class="table table-hover">
-                <thead class="table-dark">
-                    <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Price</th>
-                    <th scope="col">Category</th>
-                    <th scope="col">Action</th>
+        <div class='card mx-auto w-75'>
+            <!-- add item button -->
+            <h1 class="card-header d-flex">Current Items   
+                    <a href="additem.php" class="btn btn-success ms-auto">Add Item</a>
+            </h1>
+            <br>
+            <div class="container">
+                <table class="table table-hover">
+                    <thead class="table-dark">
+                        <!-- table headers -->
+                        <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Category</th>
+                        <th scope="col">Action</th>
 
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                        $query = "select * from items";
-                        $result = mysqli_query($conn, $query);
-                        while ($row = mysqli_fetch_assoc($result)) {
-                            ?>
-                            <tr>
-                            <th scope="row"><?php echo $row['item_id'] ?></th>
-                            <td><?php echo $row['item_name'] ?></td>
-                            <td><?php echo $row['item_price'] ?></td>
-                            <td><?php echo $row['category'] ?></td>
-                            <td>
-                            <a class="btn btn-dark" href="item.php?id=<?php echo $row['item_id']?>">Edit</a>
-                            </td>
-                            </tr>
-                            <?php
-                        }
-                    ?>
-                </tbody>
-                </table>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <!-- query to get the items from database and display them onto the item screen -->
+                        <?php
+                            $query = "select * from items";
+                            $result = mysqli_query($conn, $query);
+                            while ($row = mysqli_fetch_assoc($result)) {
+                                ?>
+                                <tr>
+                                <th scope="row"><?php echo $row['item_id'] ?></th>
+                                <td><?php echo $row['item_name'] ?></td>
+                                <td><?php echo $row['item_price'] ?></td>
+                                <td><?php echo $row['category'] ?></td>
+                                <td>
+                                <!-- button to edit and redirect to a screen where you can edit the item chosen -->
+                                <a class="btn btn-dark" href="item.php?id=<?php echo $row['item_id']?>">Edit</a>
+                                </td>
+                                </tr>
+                                <?php
+                            }
+                        ?>
+                    </tbody>
+                    </table>
+            </div>
         </div>
+        
     </body>
 </html>
