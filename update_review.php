7e8fb181b94ec2c044f7bf6bcd7c8a5bbf349756
Annie Ren <anniexren@gmail.com>
2023-05-05 16:58:59 -0500

    update not working

--- update_review.php
+++ update_review.php
@@ -16,24 +16,22 @@ session_start();
 
     if($_SERVER['REQUEST_METHOD'] == 'POST') {
         //something was posted
-        $name = $_POST['item_name'];
-        $price = $_POST['item_price'];
-        $category = $_POST['category'];
+        $review_id = $_POST['review_id'];
+        $rating = $_POST['rating'];
+        $description = $_POST['description'];
 
-        $valid_entries = True;
-
-        if($valid_entries) {
-            //save to database
-            
-            $query = "update `reviews` set rating='$rating',description='$description' WHERE review_id = '$review_id'";
-            
-            $result = mysqli_query($conn, $query);
-            if ($result) {   
-                echo "Updated successfully";
-                header('Location: all_reviews.php');
-            }
-            die;
+      
+        //save to database
+        
+        $query = "update `reviews` set rating='$rating',description='$description' WHERE review_id = '$review_id'";
+        
+        $result = mysqli_query($conn, $query);
+        if ($result) {   
+            echo "Updated successfully";
+            header('Location: all_reviews.php');
         }
+        die;
+        
     }
 ?>
 
@@ -48,29 +46,25 @@ session_start();
 <div> 
 <section class="account-form">
 
-   <?php
-       $query = "update `review` set rating='$rating',description='$description' WHERE review_id = '$id'";
-        while($fetch_review = $select_review->fetch()){
-   ?>
-   <form action="" method="post">
-      <h3>edit your review</h3>
-      <input type="text" name="title" required maxlength="50"class="box"?>">
-      <p >review description</p>
-      <textarea name="description" class="box" maxlength="1000" cols="30" rows="10"><?= $fetch_review['description']; ?></textarea>
-      <p >review rating <span></span></p>
-      <select name="rating" class="box" required>
-         <option value="<?= $fetch_review['rating']; ?>"><?= $fetch_review['rating']; ?></option>
-         <option value="1">1</option>
-         <option value="2">2</option>
-         <option value="3">3</option>
-         <option value="4">4</option>
-         <option value="5">5</option>
-      </select>
-      <input type="submit" value="update review" name="submit" class="btn">
-   </form>
-   <?php
-         }
-   ?>
+
+<section class="account-form">
+
+        <form action="" method="post">
+        <h3>Update Your Review</h3>
+        <p>Review description</p>
+        <textarea name="description" class="box" value= <?php echo $row['description']?> maxlength="1000" cols="30" rows="10"></textarea>
+        <p>Review Rating <span></span></p>
+        <select name="rating" class="box" required>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+        </select>
+        <input type="submit" value="submit review" name="submit" class="btn">
+        </form>
+
+</section>
 
 </section>
 </div>
